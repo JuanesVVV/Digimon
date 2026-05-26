5249c0ab8b9021dd9544dcbf07e6788ed3de1898
Juan Esteban <102327436+JuanesVVV@users.noreply.github.com>
2026-05-26 05:15:50 +0000

    vite2.0

--- Digimon/vite.config.ts
+++ Digimon/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Digimon/',
+  base: '/Digimon/Digimon/',
   build: {
     outDir: 'docs',
   },
